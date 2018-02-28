02664451c7acc4f835ad33a6730517b6990271ab
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-02-28 01:04:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
+ describe('johnLennonFacts', function(){
+    it("returns an array of strings with exclamation points", function() {
+      expect(johnLennonFacts([
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+
+      expect(johnLennonFacts([
+        "foo",
+        "bar",
+      ])).toEqual(["foo!!!", "bar!!!"])
+    });
+  });
